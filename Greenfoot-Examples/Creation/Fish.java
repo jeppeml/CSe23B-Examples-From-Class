a66f86a35133c118d80a52adf7df56c81a461cb2
jeppeml <jeppeml@users.noreply.github.com>
2023-09-08 10:58:35 +0200

    .

--- Greenfoot-Examples/Creation/Fish.java
+++ Greenfoot-Examples/Creation/Fish.java
@@ -3,13 +3,38 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Fish extends Actor
 {
     private int speed = 3;
+    private double hunger = 100.0; // 100 very hungry, 0 not hungry
+    private int hp = 500;
+    
+    public double getHunger(){
+        return hunger;
+    }
+    
+    public int getHp(){
+        return hp;
+    }
+    
     public void act() 
     {
+        hunger = hunger + 0.3;
+        
+        if(hunger>=100){
+            hunger = 100;
+            hp--;
+        }
         checkMovementKeys();
         Actor burger = getOneIntersectingObject(Hamburger.class);
         
         if(burger != null){
             getWorld().removeObject(burger);
+            hunger = hunger - 25;
+            if(hunger<0){
+                hunger = 0;
+            }
+        }
+        
+        if(hp<=0){
+            getWorld().removeObject(this);
         }
     }   
     
--- Greenfoot-Examples/Creation/MyWorld.java
+++ Greenfoot-Examples/Creation/MyWorld.java
@@ -3,11 +3,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
     private int countActs = 0;
+    private Fish fishie;
+    private Counter healthCounter = new Counter("Health:  ");
+    private Counter hungerCounter = new Counter("Hunger:  ");
+    
     public MyWorld()
     {    
         super(1200, 700, 1); 
-        Fish fishie = new Fish();
+        fishie = new Fish();
         addObject(fishie, 100, 550);
+        addObject(healthCounter, 75, 650);
+        addObject(hungerCounter, 75, 680);
     }
     
     public void act(){
@@ -16,6 +22,12 @@ public class MyWorld extends World
             spawnBurger();
             countActs=0;
         }
+        
+        double fishHunger = fishie.getHunger();
+        hungerCounter.setValue((int)fishHunger);
+        
+        int fishHp = fishie.getHp();
+        healthCounter.setValue(fishHp);
         
     }
     
