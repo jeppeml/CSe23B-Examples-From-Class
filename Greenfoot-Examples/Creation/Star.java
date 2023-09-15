b926d0f4fe9bb219dd43499a8a0846bf9c454521
jeppeml <jeppeml@users.noreply.github.com>
2023-09-15 10:43:56 +0200

    .

--- Greenfoot-Examples/Creation/Star.java
+++ Greenfoot-Examples/Creation/Star.java
@@ -9,20 +9,26 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Star extends SmoothMover
 {
     private int speed = -(Greenfoot.getRandomNumber(3)+1);
+    private double incSpeed = speed;
     private int countWait = Greenfoot.getRandomNumber(2000);
     public Star(){
         setRotation(Greenfoot.getRandomNumber(360));
     }
     public void act() 
     {
+        speed = (int)incSpeed;
+        
         countWait--;
         if(countWait<0)
         {
             move(speed);
-            if(getX()<1 || getX()>=1199 || getY()<1 || getY()>=699)
+            if(getX()<1 || getX()>=1199 || getY()<1 || getY()>=699){
                 this.setLocation(600,360);
+                speed = -(Greenfoot.getRandomNumber(3)+1);
+                incSpeed=speed;
+            }
         }
         
-        
+        incSpeed*=1.01;
     }    
 }
