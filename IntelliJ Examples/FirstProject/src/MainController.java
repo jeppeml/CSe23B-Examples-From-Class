2de0efb1597978509ded384f32135b724a3da56e
jeppeml <jeppeml@users.noreply.github.com>
2023-09-26 10:50:29 +0200

    .

--- IntelliJ Examples/FirstProject/src/MainController.java
+++ IntelliJ Examples/FirstProject/src/MainController.java
@@ -1,2 +1,15 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+import java.util.Random;
+
 public class MainController {
+    public Button btnBig;
+    Random rand = new Random();
+
+    public void click(ActionEvent actionEvent) {
+        System.out.println("Weeeeeeeee!");
+        btnBig.setLayoutX(rand.nextDouble(700));
+        btnBig.setLayoutY(rand.nextDouble(450));
+    }
 }
