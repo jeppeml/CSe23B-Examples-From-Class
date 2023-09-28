c29a0ce158af58cba47707e40ba6dd0d8323b988
jeppeml <jeppeml@users.noreply.github.com>
2023-09-28 10:47:02 +0200

    .

--- IntelliJ Examples/CalculatorExample/src/CalcController.java
+++ IntelliJ Examples/CalculatorExample/src/CalcController.java
@@ -1,3 +1,43 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class CalcController {
+
+    @FXML
+    private TextField txtDisplay;
+
+    public void click0(ActionEvent actionEvent) {
+    }
+
+    public void click1(ActionEvent actionEvent) {
+        txtDisplay.setText("1");
+    }
+
+    public void click2(ActionEvent actionEvent) {
+        txtDisplay.setText("2");
+    }
+
+    public void click3(ActionEvent actionEvent) {
+    }
+
+    public void click4(ActionEvent actionEvent) {
+    }
+
+    public void click5(ActionEvent actionEvent) {
+    }
+
+    public void click6(ActionEvent actionEvent) {
+    }
+
+    public void click7(ActionEvent actionEvent) {
+    }
+
+    public void click8(ActionEvent actionEvent) {
+    }
+
+    public void click9(ActionEvent actionEvent) {
+    }
+
 
 }
--- IntelliJ Examples/CalculatorExample/src/Main.java
+++ IntelliJ Examples/CalculatorExample/src/Main.java
@@ -6,13 +6,11 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Application.launch();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        System.out.println("Start method called!");
         FXMLLoader loader = new FXMLLoader(
                 getClass().getResource("CalcView.fxml"));
         Parent root = loader.load();
