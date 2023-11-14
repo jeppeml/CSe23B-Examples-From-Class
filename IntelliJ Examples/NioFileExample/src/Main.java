378292e505bc486a4f6af42323a29c9031a1a1c2
jeppeml <jeppeml@users.noreply.github.com>
2023-11-14 11:37:20 +0100

    .

--- IntelliJ Examples/NioFileExample/src/Main.java
+++ IntelliJ Examples/NioFileExample/src/Main.java
@@ -1,19 +1,39 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 import static java.nio.file.StandardOpenOption.APPEND;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Wombat henrik = new Wombat(666);
+        /*Wombat henrik = new Wombat(666);
         henrik.setName("Henrik 1st");
         henrik.setWeight(66);
         Wombat henrik2 = new Wombat(999,"Henrik 2nd", 89);
         WombatFileHandler wf = new WombatFileHandler();
         wf.addWombat(henrik);
-        wf.addWombat(henrik2);
-        System.out.println("Hello world!");
+        wf.addWombat(henrik2);*/
+        WombatFileHandler wf = new WombatFileHandler();
+        //List<Wombat> wombats = wf.getAllWombats();
+
+        wf.updateWombat(new Wombat(666, "Henrik VERY 1st", 42));
+/*
+        System.out.println("amount of wombats: " + wombats.size());
+        for(Wombat w : wombats){
+            System.out.println(w.getId() + ", " + w.getName()+ ", " + w.getWeight());
+        }
+
+        System.out.println("Wombat no 2: " + wf.getWombat(666).getName());
+
+        wf.deleteWombat(666);
+
+
+        System.out.println("amount of wombats: " + wombats.size());
+        for(Wombat w : wombats){
+            System.out.println(w.getId() + ", " + w.getName()+ ", " + w.getWeight());
+        }*/
+
     }
 
 
--- IntelliJ Examples/NioFileExample/src/WombatFileHandler.java
+++ IntelliJ Examples/NioFileExample/src/WombatFileHandler.java
@@ -1,15 +1,71 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
 public class WombatFileHandler {
     private String filename = "wombat.txt";
 
+    public Wombat getWombat(int id){
+        List<Wombat> wombats = getAllWombats();
+        for(Wombat w: wombats){
+            if(w.getId()==id)
+                return w;
+        }
+        return null;
+    }
+    public void updateWombat(Wombat wombat){
+        List<Wombat> wombats = getAllWombats();
+        List<String> wombatsChanged = new ArrayList<>();
+
+        for(Wombat w: wombats){
+            if(w.getId()==wombat.getId())
+                wombatsChanged.add(getStringFromWombat(wombat));
+            else
+                wombatsChanged.add(getStringFromWombat(w));
+        }
+        try {
+            Files.write(Paths.get(filename), wombatsChanged, TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void deleteWombat(int id){
+        List<Wombat> wombats = getAllWombats();
+        List<String> wombatsWithout = new ArrayList<>();
+
+        for(Wombat w: wombats){
+            if(w.getId()!=id)
+                wombatsWithout.add(getStringFromWombat(w));
+        }
+        try {
+            Files.write(Paths.get(filename), wombatsWithout, TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Wombat> getAllWombats(){
+        List<Wombat> wombats = new ArrayList();
+        try {
+            List<String> lines = Files.lines(Paths.get(filename)).toList();
+            for(String line : lines){
+                Wombat w = getWombatFromString(line);
+                wombats.add(w);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return wombats;
+    }
+
     public void addWombat(Wombat w){
         try {
-            Files.write(Paths.get("wombat.txt"), getStringFromWombat(w).getBytes(), APPEND);
+            Files.write(Paths.get(filename), getStringFromWombat(w).getBytes(), APPEND);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -22,5 +78,14 @@ public class WombatFileHandler {
         stringWombat = stringWombat + w.getWeight() + "\n";
 
         return stringWombat;
+    }
+
+    private Wombat getWombatFromString(String s){
+        String[] fields = s.split(";");
+        int id = Integer.parseInt(fields[0]);
+        String name = fields[1];
+        double weight = Double.parseDouble(fields[2]);
+        Wombat w = new Wombat(id, name, weight);
+        return w;
     }
 }
